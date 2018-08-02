2864225d170d1b0b8680e536cd70ef3ea5588e1a
John Distasio <john@sect0r7.net>
2018-08-02 13:00:46 -0400

    Streamline fish management

---
+++ roles/fish/files/config.fish
@@ -0,0 +1,7 @@
+set -xg EDITOR /usr/bin/nvim
+
+set -xg PAGER /usr/bin/less
+
+set -xg PATH $HOME/go/bin $HOME/bin $HOME/.local/bin $PATH
+
+set -xg TERM screen-256color
--- roles/fish/files/path.fish
+++
@@ -1,1 +0,0 @@
-set -xg PATH $HOME/go/bin $HOME/bin $HOME/.local/bin $PATH

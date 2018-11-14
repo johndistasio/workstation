c4518e595efaf00e5a936222649cb384f14dbde6
John Distasio <john@sect0r7.net>
2018-11-13 22:27:46 -0500

    Update fish configuration

--- roles/fish/files/env.fish
+++ roles/fish/files/env.fish
@@ -2,6 +2,8 @@ set -xg EDITOR /usr/bin/nvim
 
 set -xg PAGER /usr/bin/less
 
-set -xg PATH $HOME/go/bin $HOME/bin $HOME/.local/bin $PATH
+set -xg GOPATH $HOME/go
+
+set -xg PATH $GOPATH/bin $HOME/bin $HOME/.local/bin $PATH
 
 set -xg TERM screen-256color

205161d669a4b80804807160e0e0e757b97083dd
John Distasio <john@sect0r7.net>
2018-11-23 12:18:32 -0500

    Reorder fish environment config

--- roles/fish/files/env.fish
+++ roles/fish/files/env.fish
@@ -2,8 +2,8 @@ set -xg EDITOR /usr/bin/nvim
 
 set -xg PAGER /usr/bin/less
 
+set -xg TERM screen-256color
+
 set -xg GOPATH $HOME/go
 
 set -xg PATH $GOPATH/bin $HOME/bin $HOME/.local/bin $PATH
-
-set -xg TERM screen-256color
